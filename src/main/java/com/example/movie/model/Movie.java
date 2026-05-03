03a95e6ce4d888ed4fec175695e34389816a3be1
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-03 16:07:46 +0000

    model class committed

---
+++ src/main/java/com/example/movie/model/Movie.java
@@ -0,0 +1,53 @@
+/*
+ * You can use the following import statements
+ * 
+ * import javax.persistence.*;
+ * 
+ */
+
+// Write your code here
+
+package com.example.movie.model;
+import javax.persistence.*;
+@Entity
+@Table(name = "MOVIELIST")
+public class Movie {
+    @Id
+    @Column(name = "MOVIEID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int movieId;
+    @Column(name = "MOVIENAME")
+    private String movieName;
+    @Column(name = "LEADACTOR")
+    private String leadActor;
+
+    public Movie(int movieId, String movieName, String leadActor) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.leadActor = leadActor;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getLeadActor() {
+        return leadActor;
+    }
+
+    public void setLeadActor(String leadActor) {
+        this.leadActor = leadActor;
+    }
+}
