22719ba441b79e662a860a5dd2ef5b8bbe01001a
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-04 06:31:18 +0000

    repository.java committed

--- src/main/java/com/example/movie/repository/MovieRepository.java
+++ src/main/java/com/example/movie/repository/MovieRepository.java
@@ -1,1 +1,18 @@
 // Write your code here
+package com.example.movie.repository;
+
+import java.util.*;
+import com.example.movie.model.Movie;
+
+public interface MovieRepository {
+
+    List<Movie> MovieList();
+
+    Movie addMovie(Movie movie);
+
+    Movie getMovieById(int movieId);
+
+    Movie updateMovie(int movieId, Movie movie);
+
+    void deleteMovie(int movieId);
+}
