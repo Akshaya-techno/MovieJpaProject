34de250c2036512eb60e39e4e034cc13d3418197
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-04 06:24:02 +0000

    MovieJpaREpository committed

--- src/main/java/com/example/movie/repository/MovieJpaRepository.java
+++ src/main/java/com/example/movie/repository/MovieJpaRepository.java
@@ -7,3 +7,12 @@
  */
 
 // Write your code here
+package com.example.movie.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieJpaRepository extends JpaRepository<Movie, Integer>{
+    
+}
