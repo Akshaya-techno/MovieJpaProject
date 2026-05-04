42bd41c71aa7a3c6756fb4c69743d20bfc5a6a4c
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-04 06:52:13 +0000

    controller layer committed

--- src/main/java/com/example/movie/controller/MovieController.java
+++ src/main/java/com/example/movie/controller/MovieController.java
@@ -8,3 +8,45 @@
  */
 
 // Write your code here
+package com.example.movie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.movie.model.Movie;
+import com.example.movie.service.MovieJpaService;
+
+import java.util.*;
+
+@RestController
+class MovieController {
+    @Autowired
+    private MovieJpaService movieJpaService;
+
+    @GetMapping("/movies")
+    public List<Movie> MovieList() {
+        return movieJpaService.MovieList();
+    }
+
+    @PostMapping("/movies")
+
+    public Movie addMovie(@RequestBody Movie movie) {
+        return movieJpaService.addMovie(movie);
+    }
+
+    @GetMapping("/movies/{movieId}")
+    public Movie getMovieById(@PathVariable("movieId") int movieId) {
+        return movieJpaService.getMovieById(movieId);
+    }
+
+    @PutMapping("/movies/{movieId}")
+    public Movie updateMovie(@PathVariable("movieId") int movieId,
+            @RequestBody Movie movie) {
+        return movieJpaService.updateMovie(movieId, movie);
+    }
+
+    @DeleteMapping("/movies/{movieId}")
+   
+    public void deleteMovie(@PathVariable("movieId") int movieId) {
+        movieJpaService.deleteMovie(movieId);
+    }
+}
